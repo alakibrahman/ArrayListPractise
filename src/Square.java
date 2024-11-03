305dedc0058333aed649cebeed377f67bfb1cd4b
Al Akib <alakibrahman@gmail.com>
2024-11-03 19:58:16 +0600

    Arraylist

---
+++ src/Square.java
@@ -0,0 +1,124 @@
+
+public class Square {
+    public static void main(String[] args) {
+
+//
+//        // remove all element of an arrayList
+//        ArrayList<String> list1 = new ArrayList<>();
+//        list1.add("liton");
+//        list1.add("mukti");
+//        list1.add("limu");
+//        list1.add("shyma");
+//        System.out.println("Before removing : " + list1);
+//        list1.removeAll(list1);
+//        System.out.println("After removing : " + list1);
+
+
+//        // copy one arraylist to another
+//        ArrayList<String> list2 = new ArrayList<>();
+//        list2.add("liton");
+//        list2.add("mukti");
+//        list2.add("limu");
+//        list2.add("shyma");
+//        ArrayList<String> list2 = new ArrayList<>();
+//        list2.addAll(list2);
+
+
+
+        //testing two arrayLists are same or not
+//        ArrayList<String> list2 = new ArrayList<>();
+//        list2.add("liton");
+//        list2.add("mukti");
+//        list2.add("limu");
+//        list2.add("shyma");
+//        ArrayList<String> list2 = new ArrayList<>();
+//        list2.add("liton");
+//        list2.add("mukti");
+//        list2.add("limu");
+//        list2.add("shyma");
+//
+//        boolean areEqual;
+//
+//        areEqual = list2.equals(list2);
+//        System.out.println("Two arraylist are same it's " + areEqual);
+
+
+        //Checker board pattern
+//        int [] [] a = new int[10][10];
+//        for(int i = 0; i < 3; i++) {
+//            for(int j = 0; j < 3; j++) {
+//                if(j % 2 == 0 && i % 2 == 0) {
+//                    a[i][j] =  1;
+//                }
+//                else if(i % 2 != 0 && j % 2 != 0) {
+//                    a[i][j] = 1;
+//                }
+//                else {
+//                    a[i][j] = 0;
+//                }
+//            }
+//        }
+//        for(int i = 0; i < 3; i++) {
+//            for(int j = 0; j < 3; j++) {
+//                System.out.print(a[i][j] + "\t");
+//            }
+//            System.out.println();
+//        }
+
+        //arrayList
+//        ArrayList<Integer> list = new ArrayList<Integer>();
+//
+//        for(int i = 1; i <= 10; i++) {
+//            list.add(i - 1, i * i);
+//        }
+//        System.out.println(list);
+
+        // Array
+//        int[] a = new int[5];
+//        int m = 0;
+//        int i = 0;
+//        int sum = 0;
+//
+//        Scanner in = new Scanner(System.in);
+//        for(int n : a) {
+//            a[i] = in.nextInt();
+//            i++;
+//        }
+//        int t = 4;
+//       for(int n : a) {
+//           if(n == t) {
+//               System.out.println(m);
+//           }
+//           m++;
+//       }
+//        System.out.println(sum);
+
+
+
+//        for(int n : a) {
+//            if(n < 0) {
+//                m++;
+//            }
+//        }
+//        System.out.println(m);
+//
+//        int length = a.length;
+//        System.out.println("length of an array: " + length);
+
+//        int max = a[0];
+//
+//        for (int s : a) {
+//            if(max < s) {
+//                max = s;
+//            }
+//        }
+//        for(int m : a) {
+//            if(m == max) {
+//                System.out.println(m + "==>MAX NUMBER");
+//            }
+//            else {
+//                System.out.println(m);
+//            }
+//        }
+    }
+}
